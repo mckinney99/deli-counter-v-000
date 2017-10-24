4b710a782cd9e792efe597d3a2b29fbcef490a9e
Eric McKinney <mckinney99@gmail.com>
2017-10-24 21:03:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,3 +17,12 @@ def take_a_number(katz_deli, name)
   katz_deli << "#{name}"
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
   end
+
+def now_serving
+  if katz_deli.size < 1
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "#{katz_deli[0]}"
+  end
+  katz_deli.unshift
+end
